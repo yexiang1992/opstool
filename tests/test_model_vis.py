4aef144e1c71ce26756ba790de4f9ada634f0148
yan <yexiang_yan@yeah.net>
2022-12-02 21:50:10 +0800

    test

--- tests/test_model_vis.py
+++ tests/test_model_vis.py
@@ -4,10 +4,10 @@ from opstool.vis import GetFEMdata, OpenSeesVis
 from opstool import load_ops_examples
 
 
-load_ops_examples("ArchBridge")
+# load_ops_examples("ArchBridge")
 # load_ops_examples("CableStayedBridge")
 # load_ops_examples("Dam")
-# load_ops_examples("Frame3D")
+load_ops_examples("Frame3D")
 # load_ops_examples("Igloo")
 # load_ops_examples("Pier")
 # load_ops_examples("SuspensionBridge")
@@ -22,14 +22,14 @@ opsv.model_vis(show_node_label=False, show_ele_label=False,
                show_outline=True,
                opacity=1.0,
                save_html='ModelVis.html')
-# opsv.eigen_vis(mode_tags=[1, 9], subplots=True,
-#                alpha=None, show_outline=False,
-#                show_origin=False, opacity=1.0,
-#                show_face_line=False, save_html="EigenVis")
-# opsv.eigen_anim(mode_tag=1, alpha=None, show_outline=False,
-#                 opacity=1, framerate=3,
-#                 show_face_line=True,
-#                 save_html="EigenAnimation")
+opsv.eigen_vis(mode_tags=[1, 9], subplots=True,
+               alpha=None, show_outline=False,
+               show_origin=False, opacity=1.0,
+               show_face_line=False, save_html="EigenVis")
+opsv.eigen_anim(mode_tag=1, alpha=None, show_outline=False,
+                opacity=1, framerate=3,
+                show_face_line=True,
+                save_html="EigenAnimation")
 
 # responses
 gen_grav_load(ts_tag=10, pattern_tag=10,
@@ -47,17 +47,17 @@ ops.analysis('Static')
 
 ModelData.reset_steps_state()
 for i in range(Nsteps):
-    ops.analyze(1)
+    a = ops.analyze(1)
     ModelData.get_node_resp_step(analysis_tag=1,
                                  num_steps=Nsteps,
                                  model_update=False)
     ModelData.get_frame_resp_step(analysis_tag=1, num_steps=Nsteps,)
-# opsv.deform_vis(analysis_tag=1, slider=True,
-#                 response="disp", alpha=None,
-#                 show_outline=False, show_origin=True,
-#                 show_face_line=False, opacity=1,
-#                 save_html="DefoVis",
-#                 model_update=False)
+opsv.deform_vis(analysis_tag=1, slider=True,
+                response="disp", alpha=None,
+                show_outline=False, show_origin=True,
+                show_face_line=False, opacity=1,
+                save_html="DefoVis",
+                model_update=False)
 # opsv.deform_anim(analysis_tag=1,
 #                  response="disp", alpha=None,
 #                  show_outline=False,
