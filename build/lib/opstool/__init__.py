f525aeed3a7655ceef849ac32645ae88ce13726c
yan <yexiang_yan@yeah.net>
2023-01-06 11:44:47 +0800

    none

--- build/lib/opstool/__init__.py
+++
@@ -1,7 +0,0 @@
-from .__about__ import __version__
-from .utils import load_ops_examples
-
-__all__ = [
-    "load_ops_examples",
-    "__version__",
-]
--- build/lib/opstool/vis/__init__.py
+++
@@ -1,9 +0,0 @@
-from .get_model_data import GetFEMdata
-from .ops_vis_plotly import OpsVisPlotly
-from .ops_vis_pyvista import OpsVisPyvista
-from .fiber_sec_vis import FiberSecVis
-
-__all__ = ["GetFEMdata",
-           "OpsVisPlotly",
-           "OpsVisPyvista",
-           "FiberSecVis"]
