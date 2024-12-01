482713faa4ca5c285dd9943d310420295312059b
Yexiang <yexiang_yan@outlook.com>
2024-11-30 20:26:29 -0500

    feat: add transform_surface_uniform_load()

--- opstool/pre/__init__.py
+++ opstool/pre/__init__.py
@@ -2,7 +2,7 @@ from ._read_gmsh import Gmsh2OPS
 from ._model_mass import ModelMass
 from .pre_utils import remove_void_nodes
 from ._load import gen_grav_load, create_gravity_load
-from ._load import transform_beam_uniform_load, transform_beam_point_load
+from ._load import transform_beam_uniform_load, transform_beam_point_load, transform_surface_uniform_load
 from .tcl2py import tcl2py
 from ._unit_system import UnitSystem
 from . import section
@@ -16,6 +16,7 @@ __all__ = [
     "create_gravity_load",
     "transform_beam_uniform_load",
     "transform_beam_point_load",
+    "transform_surface_uniform_load",
     "tcl2py",
     "UnitSystem"
 ]
