3ab973356fc8721bf92ae260b6e553dc77281409
Yexiang <yexiang_yan@outlook.com>
2024-12-13 23:25:43 -0500

    feat: add contact element tags

--- opstool/utils/ops_ele_class_tags.py
+++ opstool/utils/ops_ele_class_tags.py
@@ -439,6 +439,7 @@ ELE_TAG_Brick = [
 ]
 ELE_TAG_PFEM = [133, 141, 142, 143, 144, 164, 187, 189, 199, 200, 255]
 ELE_TAG_UP = [40, 46, 47, 48, 50, 51, 120, 122]
+ELE_TAG_CONTACT = [22, 23, 24, 113, 114, 115, 117, 118, 123, 124, 125]
 
 OPS_ELE_TAGS = SimpleNamespace(
     Types=ELE_TYPE_TAGS,
@@ -454,7 +455,8 @@ OPS_ELE_TAGS = SimpleNamespace(
     Shell=ELE_TAG_Shell,
     SurfaceLoad=ELE_TAG_SURFACE_LOAD,
     # solid-fluid coupled elements, solid displacement (u) and fluid pressure (p)
-    UP=ELE_TAG_UP
+    UP=ELE_TAG_UP,
+    CONTACT=ELE_TAG_CONTACT,
 )
 OPS_ELE_CLASSTAG2TYPE = ELE_TYPE_TAGS
 
