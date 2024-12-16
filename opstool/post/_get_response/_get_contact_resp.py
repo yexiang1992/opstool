dba480562fc22232971764192601454c9de8571f
Yexiang <yexiang_yan@outlook.com>
2024-12-15 19:22:52 -0500

    feat: add global forces for contact elements

--- opstool/post/_get_response/_get_contact_resp.py
+++ opstool/post/_get_response/_get_contact_resp.py
@@ -9,7 +9,7 @@ class ContactRespStepData(ResponseBase):
 
     def __init__(self, ele_tags=None):
         self.resp_names = [
-            "localForces", "localDisp", "slips"
+            "globalForces", "localForces", "localDisp", "slips"
         ]
         self.resp_steps = None
         self.step_track = 0
@@ -28,9 +28,10 @@ class ContactRespStepData(ResponseBase):
 
     def add_data_one_step(self, ele_tags):
         with suppress_ops_print():
-            forces, defos, slips = _get_contact_resp(ele_tags)
+            global_forces, forces, defos, slips = _get_contact_resp(ele_tags)
         data_vars = {}
         if len(ele_tags) > 0:
+            data_vars["globalForces"] = (["eleTags", "globalDOFs"], global_forces)
             data_vars["localForces"] = (["eleTags", "localDOFs"], forces)
             data_vars["localDisp"] = (["eleTags", "localDOFs"], defos)
             data_vars["slips"] = (["eleTags", "slipDOFs"], slips)
@@ -38,16 +39,21 @@ class ContactRespStepData(ResponseBase):
                 data_vars=data_vars,
                 coords={
                     "eleTags": ele_tags,
+                    "globalDOFs": ["Px", "Py", "Pz"],
                     "localDOFs": ["N", "Tx", "Ty"],
                     "slipDOFs": ["Tx", "Ty"],
                 },
                 attrs={
+                    "Px": "Global force in the x-direction on the constrained node",
+                    "Py": "Global force in the y-direction on the constrained node",
+                    "Pz": "Global force in the z-direction on the constrained node",
                     "N": "Normal force or deformation",
                     "Tx": "Tangential force or deformation in the x-direction",
                     "Ty": "Tangential force or deformation in the y-direction",
                 }
             )
         else:
+            data_vars["globalForces"] = xr.DataArray([])
             data_vars["localForces"] = xr.DataArray([])
             data_vars["localDisp"] = xr.DataArray([])
             data_vars["slips"] = xr.DataArray([])
@@ -96,37 +102,53 @@ class ContactRespStepData(ResponseBase):
 
 
 def _get_contact_resp(link_tags):
-    defos, forces, slips = [], [], []
+    defos, forces, slips, global_forces = [], [], [], []
     for etag in link_tags:
         etag = int(etag)
+        global_fo = _get_contact_resp_by_type(
+            etag, ("force", "forces"), type_="global"
+        )
         defo = _get_contact_resp_by_type(
-            etag,("localDisplacement", "localDispJump"),
+            etag,("localDisplacement", "localDispJump"), type_="local"
         )
         force = _get_contact_resp_by_type(
-            etag, ("localForce", "localForces", "forcescalars", "forcescalar")
+            etag, ("localForce", "localForces", "forcescalars", "forcescalar"),
+            type_ = "local"
         )
-        slip = _get_contact_resp_by_type(etag, ("slip",), slip=True)
+        slip = _get_contact_resp_by_type(etag, ("slip",), type_="slip")
+        global_forces.append(global_fo)
         defos.append(defo)
         forces.append(force)
         slips.append(slip)
-    return forces, defos, slips
+    return global_forces, forces, defos, slips
 
 
-def _get_contact_resp_by_type(etag, etypes, slip=False):
+def _get_contact_resp_by_type(etag, etypes, type_="local"):
     etag = int(etag)
     resp = []
     for name in etypes:
         resp = ops.eleResponse(etag, name)
         if len(resp) > 0:
             break
-    if not slip:
+    if type_ == "local":
         if len(resp) == 0:
             resp = [0.0] * 3
         elif len(resp) == 2:
             resp = [resp[0], resp[1], 0.0]
         else:
             resp = [resp[0], resp[1], resp[2]]
-    else:
+    elif type_ == "global":
+        if len(resp) == 0:
+            resp = [0.0] * 3
+        elif len(resp) == 2:
+            resp = [resp[0], resp[1], 0.0]
+        elif len(resp) == 4:
+            resp = [resp[-2], resp[-1], 0.0]
+        elif len(resp) == 6:
+            resp = [resp[-3], resp[-2], resp[-1]]
+        else:
+            resp = [resp[-3], resp[-2], resp[-1]]
+    elif type_ == "slip":
         if len(resp) == 0:
             resp = [0.0] * 2
         elif len(resp) == 1:
